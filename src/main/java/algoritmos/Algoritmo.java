311b8d9570365bcfaa46e8947b38861b8793675d
juanmartin1892 <juanmartin1892@hotmail.es>
2014-10-08 16:16:03 +0200

    Arreglando el algoritmo

--- src/main/java/algoritmos/Algoritmo.java
+++ src/main/java/algoritmos/Algoritmo.java
@@ -16,6 +16,8 @@ public class Algoritmo {
 	//Recorreremos toda esa colección, de forma que obtengamos los nombres de cada votación, y los almacenaremos en un hashset (para que no se repitan valores)
 	
 	//La supuesta lista que nos pasan
+	
+	public void Algoritmo(){
 	List<Voto> lista = new ArrayList<Voto>();
 	
 	//Suponemos que la coleccion "lista" es lo que hemos recuperado de la base de datos
@@ -27,13 +29,13 @@ public class Algoritmo {
 		claves.add(s.getNombre());
 	}
 	//Ahora, una vez tenemos las claves, crearemos un mapa al que asignaremos a cada clave su valor.
-	Map<String,Integer> resultados = new HashMap<String,Integer>();
+	Map<Voto,Integer> resultados = new HashMap<Voto,Integer>();
 
-	for(String s1: claves){
+	for(Voto s1: claves){
 		resultados.put(s1,0);
 	}
 
-	for(String s2:lista){
+	for(Voto s2:lista){
 		for (int i=0;i<resultados.keySet().size();i++){
 			if (s2.equals(resultados.get(i))){
 				resultados.put(s2,resultados.get(s2));
