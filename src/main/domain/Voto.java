0bd3ee611b66778406974ef689e5cbda04b77e71
iyoque <iyoque@gmail.com>
2014-10-08 10:21:04 +0200

    uploaded the entity "Voto"

---
+++ src/main/domain/Voto.java
@@ -0,0 +1,37 @@
+package com.egc.test;
+
+public class Voto {
+	
+	private String nombre; //This string returns the name of the vote, not the voter
+	private Integer edad;
+	private String poblacion;
+
+	
+	//Getters and setters
+	
+	public String getNombre()
+	{
+		return this.nombre;
+	}
+	public void setNombre(String n)
+	{
+		this.nombre=n;
+	}
+	
+	public Integer getEdad()
+	{
+		return this.edad;
+	}
+	public void setEdad(Integer e)
+	{
+		this.edad=e;
+	}
+	public String getPoblacion()
+	{
+		return this.poblacion;
+	}
+	public void setPoblacion(String p)
+	{
+		this.poblacion=p;
+	}
+}
