fae4c61065bfe7b048c53ba6c76ecc4a19ed9023
Adrián <Adrin@Adrin-PC>
2014-10-15 14:40:50 +0200

    VotoA

--- src/main/java/domain/Voto.java
+++ src/main/java/domain/Voto.java
@@ -4,6 +4,7 @@ public class Voto {
 
 	private String nombre; // This string returns the name of the vote, not the
 							// voter
+	private String region;
 	private Integer edad;
 	private String poblacion;
 
@@ -31,5 +32,13 @@ public class Voto {
 
 	public void setPoblacion(String p) {
 		this.poblacion = p;
+	}
+
+	public String getRegion() {
+		return region;
+	}
+
+	public void setRegion(String region) {
+		this.region = region;
 	}
 }
