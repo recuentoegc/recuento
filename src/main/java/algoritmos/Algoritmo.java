59d24b848c22ab85ea38714373a7d5aab6d7219b
iyoque <iyoque@gmail.com>
2014-12-11 10:51:22 +0100

    Creado el método "algoritmo2" que nos permite en el caso de que no se nos de una lista de strings, sino una lista de votos, realizar la misma funcionalidad que "algoritmo".

--- src/main/java/algoritmos/Algoritmo.java
+++ src/main/java/algoritmos/Algoritmo.java
@@ -7,10 +7,8 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import org.springframework.http.converter.FormHttpMessageConverter;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
+import domain.Voto;
+import domain.VotoAntiguo;
 
 public class Algoritmo {
 	// Suponemos que la base de datos almacenará la información de la siguiente
@@ -24,7 +22,7 @@ public class Algoritmo {
 
 	public static Map<String, Integer> Algoritmo(List<String> votos) {
 
-		// Suponemos que la coleccion "lista" es lo que hemos recuperado de la
+		// Suponemos que la coleccion "votos" es lo que hemos recuperado de la
 		// base de datos
 
 		Set<String> claves = new HashSet<String>();
@@ -36,9 +34,12 @@ public class Algoritmo {
 		// asignaremos a cada clave su valor.
 		Map<String, Integer> resultados = new HashMap<String, Integer>();
 
+		//Iniciamos el mapa con los valores a cero
 		for (String s1 : claves) {
 			resultados.put(s1, 0);
 		}
+		//Recorremos la lista de votos y adjudicamos el número de veces repetida a su
+		//nombre
 		for (String s2 : votos) {
 			if (resultados.containsKey(s2)) {
 				resultados.put(s2, resultados.get(s2) + 1);
@@ -48,4 +49,30 @@ public class Algoritmo {
 		return resultados;
 	}
 
+	
+	//En el caso de que no se nos pase una lista de strings, sino una lista de votos
+	//se usará este algoritmo, que hace lo mismo que el algoritmo de arriba, pero sacando de la
+	//entidad "VotoAntiguo" su nombre.
+	public static Map<String,Integer> algoritmo2(List<VotoAntiguo> votos)
+	{
+		Set<String> claves = new HashSet<String>();
+		List<String> votaciones = new ArrayList<String>();
+		for (VotoAntiguo v:votos)
+		{
+			votaciones.add(v.getNombre());
+		}
+		for (String s : votaciones) {
+			claves.add(s);
+		}
+		Map<String, Integer> resultados = new HashMap<String,Integer>();
+		for (String s1 : claves) {
+			resultados.put(s1, 0);
+		}
+		for (String s2 : votaciones) {
+			if (resultados.containsKey(s2)) {
+				resultados.put(s2, resultados.get(s2) + 1);
+			}
+		}
+		return resultados;
+	}
 }
